d0c07ede1065f174a9253a1513a54d82982445fa
Avery Yang <averyyang2674@gmail.com>
2022-06-12 15:35:52 -0500

    comments for starting server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const express = require('express');
 // 2. create variable that we can call methods on
 const app = express();
 
-//.listen(port) => builds the server!
+//.listen(port, callback function()) => builds the server!
 //port is like a 'channel' for a TV. 3000 is a port that our app is tuned into
 app.listen(3000, function () {
-    console.log('Server started on port 3000');
+    console.log('Your server has started on port 3000!');
 });
 
 //GET request => HOME route
